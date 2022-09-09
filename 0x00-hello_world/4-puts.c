87d08a38daef80244b70527d771f15cdf4c1747a
Mathewclementina <clementinemathew6628@gmail.com>
2022-09-08 17:56:42 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+
+/**
+ * main - print the string in the put function
+ * 
+ * Return:zero if no erros, return non-zero if errors
+ */
+int main(void)
+{
+	puts)"\"Programming is like building a multilingual puzzle");
+	return (0);
+}
