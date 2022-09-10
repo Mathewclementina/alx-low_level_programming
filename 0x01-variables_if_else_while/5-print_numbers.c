02795c8903c3dbc0cc6e6fe39cb3cf23c78e6bc2
Mathewclementina <clementinemathew6628@gmail.com>
2022-09-09 18:53:50 -0700

    Numbers;

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = o; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	printf('\n');
+	return (0);
+}
