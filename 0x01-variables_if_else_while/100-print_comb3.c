5b4552fc724ef89b5a6de46bcbd8358729147bab
Mathewclementina <clementinemathew6628@gmail.com>
2022-09-09 19:40:04 -0700

    Combinations of two digit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	int i, n;
+
+	for (i = '48'; i <= '56'; i++)
+	{
+		for (n = '49'; <= '57'; n++)
+		{
+			if (n > i)
+			{
+				putchar(i);
+				putchar(n);
+				if (i != 56 || n != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
