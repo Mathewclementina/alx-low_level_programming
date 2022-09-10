1285468d03abc3cc43ae08c2073fa2b7de3b5379
Mathewclementina <clementinemathew6628@gmail.com>
2022-09-09 18:08:12 -0700

    All letters except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdlid.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		if (low != 'e' && low != 'q')
+		{
+			 putchar(low);
+		}
+	}
+		putchar('\n');
+		return (0);
+}
+
+
