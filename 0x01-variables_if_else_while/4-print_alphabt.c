5106e66d0fbb32c9b5a856bc65301ff05b5b8d29
Mathewclementina <clementinemathew6628@gmail.com>
2022-09-09 18:45:58 -0700

    All letters except q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,23 +1,22 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+
 /**
  * main - main block
  * Return: 0
  */
 int main(void)
 {
-	char low;
+	char low, e, q;
+
+	e = 'e';
+	q = 'q';
 
 	for (low = 'a'; low <= 'z'; low++)
 	{
-		if (low != 'e' && low != 'q')
-		{
-			 putchar(low);
-		}
+		if (low != e && low != q)
+			putchar(low);
 	}
-		putchar('\n');
-		return (0);
+	putchar('\n');
+	return (0);
 }
-
 
