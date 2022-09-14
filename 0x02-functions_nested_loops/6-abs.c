0552d06bfa9134bc5ebeef2ee537de680afb7b70
Mathewclementina <clementinemathew6628@gmail.com>
2022-09-13 18:29:36 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _abs - function to compute tne absolute value of a number
+ * @r: argument function
+ * Return: 0
+ */
+int _abs(int r)
+{
+	if (r < 0)
+		r = -r;
+	return (r);
+}
