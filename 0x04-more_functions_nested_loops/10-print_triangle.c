5baf25680c9c0530171feaa182527398a455889f
Mathewclementina <clementinemathew6628@gmail.com>
2022-09-15 18:42:25 -0700

    Task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_triangle -> print a triangle
+ * @size: size of the shape
+ * Return: o
+ */
+void print_triangle(int size)
+{
+
+	int l, b;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	els
+		for (l = 0; l <= (size - 1); l++)
+		{
+			for (b = 0; b < (size - 1); b++)
+				_putchar('#');
+		}
+	if (l == size)
+	{
+		continue;
+	}
+	_putchar('\n');
+}
+
