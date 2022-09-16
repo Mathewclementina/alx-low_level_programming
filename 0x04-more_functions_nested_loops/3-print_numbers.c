8076715c5da87d388c29559dc38aca5df807b749
Mathewclementina <clementinemathew6628@gmail.com>
2022-09-15 19:26:51 -0700

    Task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,12 +2,14 @@
 #include <stdio.h>
 
 /**
- * print_numbers -> function that prints the numbers, from 0 to 9
+ * print_numbers -> function that prints the numbers
+ * 0 - 9
+ *
  * Return: 0
  */
+
 void print_numbers(void)
 {
-
 	int a;
 
 	for (a = 48; a <= 58; a++)
