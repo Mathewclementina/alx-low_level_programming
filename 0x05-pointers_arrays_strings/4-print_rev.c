61f3040ae32469b3f6b9d70323c08f49921dc9d0
Mathewclementina <clementinemathew6628@gmail.com>
2022-09-19 12:32:30 -0700

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,11 +1,17 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_rev _> print a string
+ * print_rev -> print a string
  * @s: string
  * Return: 0
  */
 
 void print_rev(char *s)
 {
+	int len = strlen(s);
 
+	while (len--)
+		putchar(*(s + len));
+	putchar(10);
+}
