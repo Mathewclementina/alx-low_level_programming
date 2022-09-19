ef38d0bb3d76ab604803070db5cbccea45712d66
Mathewclementina <clementinemathew6628@gmail.com>
2022-09-19 13:22:01 -0700

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *  print_array -> print element of an array of an integer
+ *  @a: var1
+ *  @n: var2
+ *  Return: 0
+ */
+
+void print_array(int *a, int n)
+{
+	int v;
+
+	int v;
+
+	for (v = 0; v < n; v++)
+	{
+		if (v != n - 1)
+			printf("%d, ", a[v]);
+		else
+			printf("%d", a[v]);
+	}
+	putchar(10);
+}
