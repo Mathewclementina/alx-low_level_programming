17a68272f14e6b557c39932da71b7c2668e9eb19
Mathewclementina <clementinemathew6628@gmail.com>
2022-09-19 12:13:45 -0700

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -8,12 +8,12 @@
 
 void _puts(char *str)
 {
-	int i = 0;
+	int v = 0;
 
-	while (*(str + i) != '\0')
+	while (*(str + v) != '\0')
 	{
-		putchar(*(str + i));
-		i++;
+		putchar(*(str + v));
+		v++;
 	}
-	_putchar(10);
+	putchar(10);
 }
