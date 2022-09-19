83d7cba904cfd05fbdf5179fb8a1893853e3f4b9
Mathewclementina <clementinemathew6628@gmail.com>
2022-09-19 12:02:05 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts -> print a string
+ * @str: parameter
+ * Return: 0
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		_putchar(*)str + i));
+		i++;
+	}
+	_putchar(10);
+}
