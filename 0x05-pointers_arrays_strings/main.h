667557dea200ec6cb4152395a5ec3891cf5089e7
Mathewclementina <clementinemathew6628@gmail.com>
2022-09-19 19:49:34 -0700

    task 10

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,7 +7,7 @@
  */
 
 void reset_to_98(int *n);
-void swap_int(int *a, int *b);
+void swap_int(int *a, int *b)g
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
@@ -16,5 +16,6 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 
 #endif /*MAIN_H*/
