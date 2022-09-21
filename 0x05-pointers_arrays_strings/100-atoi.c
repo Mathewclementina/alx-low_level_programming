48524070cd8b06fdf8317cc23217420ae7db73b4
Mathewclementina <clementinemathew6628@gmail.com>
2022-09-20 18:50:05 -0700

    task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -15,16 +15,15 @@ int _atoi(char *s)
 	i = 1;
 	var = 0;
 	do {
-		if (*s == '_')
+		if (*s == '-')
 			i *= -1;
 		else
 			if (*s >= '0' && *s <= '9')
-				var = var + 10 + (*s - '0');
+				var = var * 10 + (*s - '0');
 			else
 				if (var > 0)
 					break;
-	}
-	while (s++);
+	} while (*s++);
 
 		return (var * i);
 }
