1f7a74fc4d3ac9bf903971cbf863e3ed6643284a
Mathewclementina <clementinemathew6628@gmail.com>
2022-09-22 17:30:27 -0700

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strcmp -> compare string
+ * @s1: first string
+ * @s2: second string
+ * Return: 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int l = 0, diff = 0;
+
+	while (1)
+	{
+		if (s1[l] == '\0' && s2[l] == '\0')
+			break;
+		else if (s1[l] == '\0')
+		{
+			diff = s2[l];
+			break;
+		}
+		else if (s2[l] == '\0')
+		{
+			diff = s1[l]
+				bresk;
+		}
+		else if (s1[l] != s2[l])
+		{
+			diff = s1[l] - s2[l];
+			break;
+		}
+		else
+			l++;
+	}
+	return (diff);
+}
