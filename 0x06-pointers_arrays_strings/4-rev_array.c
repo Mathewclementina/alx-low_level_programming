815c30fc44eed9ee05990a399952ae62b4e53553
Mathewclementina <clementinemathew6628@gmail.com>
2022-09-22 17:09:23 -0700

    task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * reverse_array -> reverse content of an array of integer
+ * @a: array
+ * @n: integer
+ */
+
+void reverse_array(int *a, int n)
+{
+	int tmp, index;
+
+	for (index = n - 1; index > n / 2; index--)
+	{
+		tmp = a[n - 1 - index];
+		a[n - 1 - index] = a[index];
+		a[index] = tmp;
+	}
+}
