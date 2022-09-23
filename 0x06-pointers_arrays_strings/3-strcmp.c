dc64b9ce3f9d4909d3e439a4e627140885b2c7e5
Mathewclementina <clementinemathew6628@gmail.com>
2022-09-22 17:35:59 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,8 +6,8 @@
  * @s2: second string
  * Return:
  *                returns zero if s1 == s2
- *                returns negative if s1 < s2
- *                returns positive if s1 > s2
+ *                returns negative number if s1 < s2
+ *                returns positive number if s1 > s2
  */
 
 int _strcmp(char *s1, char *s2)
