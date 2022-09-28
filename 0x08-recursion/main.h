bae9231562cc6ac866b891ec463b1dcc15e60b21
Mathewclementina <clementinemathew6628@gmail.com>
2022-09-28 13:57:48 -0700

    task 0

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,11 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
+#include <unistd.h>
 /**
- * void prototypes()
- * int prototypes()
- * char prototypes()
+ * void prototypes(void)
+ * int prototypes(int)
+ * char prototypes(char)
  */
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
