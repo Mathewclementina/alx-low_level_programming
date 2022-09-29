fcbc6437221c0271ffbf0b5e390d71e9c51caffa
Mathewclementina <clementinemathew6628@gmail.com>
2022-09-28 21:11:25 -0700

    task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * is_prime_number -> return 1 if the input integer is a prime number
+ * @n: character to be used
+ * Return: 0
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (is_prime(n, start));
+}
