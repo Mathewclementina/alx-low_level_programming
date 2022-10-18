fde6a8e23079118c75a928bf55a33aa8945db0d1
Mathewclementina <clementinemathew6628@gmail.com>
2022-10-18 07:24:27 -0700

    task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main -> print all arguments it receives
+ * @argc: number
+ * @argv: argument
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	if (argc > 0)
+	{
+		while (i < argc)
+		{
+			printf("s\n", argv[i]);
+		}
+	}
+	return (0);
+}
